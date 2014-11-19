36f71585720cc657e41b6ddd56ae3f1749e5330f
Tanoy Sinha <tsinha@gmail.com>
2014-11-19 18:56:01 -0500

    remove dupes

--- 2_chapter/2_1_remove_dupes.py
+++ 2_chapter/2_1_remove_dupes.py
@@ -1,7 +1,5 @@
 from linked_list import LinkedList
 
-lst = LinkedList.build_from_array(['F', 'O', 'L', 'L', 'O', 'W', 'U', 'P'])
-
 def remove_dupes(lst):
   hsh = {}
   previous = lst.root
@@ -14,5 +12,23 @@ def remove_dupes(lst):
       previous = current
     current = current.next
 
+def remove_dupes_in_place(lst):
+  current = lst.root
+  while current:
+    runner = current.next
+    previous = current
+    while runner:
+      if runner.data == current.data:
+        previous.next = runner.next
+      else:
+        previous = previous.next
+      runner = runner.next
+    current = current.next
+
+lst = LinkedList.build_from_array(['F', 'O', 'L', 'L', 'O', 'W', 'U', 'P'])
+lst.prn()
+remove_dupes_in_place(lst)
+lst.prn()
+lst = LinkedList.build_from_array(['F', 'O', 'L', 'L', 'O', 'W', 'U', 'P'])
 remove_dupes(lst)
 lst.prn()
