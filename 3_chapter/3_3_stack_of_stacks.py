717e7442dfac74b3bf038b265db8e4a3210f61f0
Tanoy Sinha <tsinha@gmail.com>
2014-11-25 14:46:11 -0500

    completed stacks

--- 3_chapter/3_3_stack_of_stacks.py
+++ 3_chapter/3_3_stack_of_stacks.py
@@ -32,6 +32,10 @@ class SetOfStacks():
       else:
         return stack.pop()
 
+  def pop_at(self, index):
+    if index < len(self.stacks):
+      return self.stacks[index].pop()
+
 
 
 ss = SetOfStacks(2)
@@ -43,3 +47,13 @@ assert(ss.pop() == 5)
 assert(ss.pop() == 4)
 assert(len(ss.stacks) == 1)
 assert(ss.pop() == 3)
+
+ss.push(3)
+ss.push(4)
+ss.push(5)
+assert(len(ss.stacks) == 2)
+assert(ss.pop_at(0) == 4)
+assert(ss.pop_at(0) == 3)
+assert(ss.pop_at(0) == None)
+assert(ss.pop() == 5)
+assert(ss.pop() == None)
